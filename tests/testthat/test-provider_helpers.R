6d10df8677ca49c10645948c0c20d73e5ed45526
gnoblet <gnoblet@zaclys.net>
2026-02-06 12:17:21 +0100

    Remove obsolete provider helpers test

--- tests/testthat/test-provider_helpers.R
+++
@@ -1,9 +0,0 @@
-test_that("provider helpers return expected structure", {
-  p <- get_provider_details("bunny")
-  expect_true(is.list(p))
-  expect_true(!is.null(p$source))
-  expect_equal(provider_name(p), "bunny")
-  nb <- new_bunny_provider()
-  expect_true(is.list(nb))
-  expect_equal(nb$source, "bunny")
-})
